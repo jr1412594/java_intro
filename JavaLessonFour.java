649246e2ebff8c7accaed6d54b363855a31999cd
jr1412594 <jr.medina1412@gmail.com>
2021-11-16 15:12:51 -0700

    java loops

--- JavaLessonFour.java
+++ JavaLessonFour.java
@@ -21,18 +21,37 @@ public class JavaLessonFour {
         // }
 ////////////////////////////////////////////////////////////////
 
-        String contYorN = "Y";
+    //     String contYorN = "Y";
 
-        int h = 1;
+    //     int h = 1;
 
-        while (contYorN.equalsIgnoreCase("y"))
-        {
-            System.out.println(h);
-            System.out.print("Continue y or n? ");
-            contYorN = userInput.nextLine();
-            h++;
-        }
+    //     while (contYorN.equalsIgnoreCase("y"))
+    //     {
+    //         System.out.println(h);
+    //         System.out.print("Continue y or n? ");
+    //         contYorN = userInput.nextLine();
+    //         h++;
+    //     }
 
+///////////////////////////////////////////////////////////////
+
+        // int k = 100;
+
+        // do
+        // {
+        //     System.out.println(k);
+        //     k++;
+        // } while (k < 10);
+
+
+////////////////////////////////////////////////////////////////
+
+            // for (declare iterator; conditional statement; change itterator)
+
+            for (int i = 10; i >= 1; i--) 
+            {
+                System.out.println(i);
+            } 
     }
 
 }
