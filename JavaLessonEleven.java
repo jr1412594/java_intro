068b13d41381aac58502498d497a9c1e14288957
jr1412594 <jr.medina1412@gmail.com>
2021-11-19 09:28:17 -0700

    ArrayList

--- JavaLessonEleven.java
+++ JavaLessonEleven.java
@@ -2,44 +2,99 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Arrays;
 
-
 public class JavaLessonEleven {
 
     public static void main(String[] args) {
 
-        ArrayList<Object> arrayListOne;
+        ArrayList arrayListOne; 
 
-        arrayListOne = new ArrayList<Object>();
+        arrayListOne = new ArrayList();
 
-        ArrayList arrayListTwor = new ArrayList();
+        ArrayList arrayListTwo = new ArrayList();
+
 
         ArrayList<String> names = new ArrayList<String>();
 
         names.add("Pierogi");
-        names.add("Miso Cat");
-        names.add("Karina Love");
+        names.add("Miso");
+        names.add("Dino");
 
-        names.add(2, "Junior");
+        names.add("Karina");
+        names.add("Junior");
 
-        // System.out.println(names.get(1) + " " + " " + names.get(2));
+        for(int i = 0; i < names.size(); i++) {
+            System.out.println(names.get(i));
+        }
 
-        // for (int i = 0; i < names.size(); i++) {
-        //     System.out.println(names.get(i));
+        // names.remove(3);
+
+
+        int k = 1;
+        while(k < 21) { System.out.print("-"); k++;}
+        System.out.println();
+
+        for (int i = 0; i < names.size(); i++) {
+            System.out.println(names.get(i));
+        }
+
+        System.out.println();
+        System.out.println(names);
+
+        // Creates an iterator object (indivItems)s
+        // Iterator indivItems = names.iterator(); 
+
+        // while(indivItems.hasNext()) {
+        //     System.out.println(indivItems.next() + " : this is the iterator");
         // }
 
-        ArrayList check = new ArrayList();
+        ArrayList nameCopy = new ArrayList();
 
-        check.add(5);
-        check.add(true);
-        check.add("Miso");
-        check.add("Pierogi");
-        check.add(3.1415);
-        check.add(null);
+        ArrayList nameBackup = new ArrayList();
 
-        check.set(0, "this was 5");
+        nameCopy.addAll(names);
 
-        for (int i = 0; i < check.size(); i++) {
-            System.out.println(check.get(i));
-        }        
+        String paulYoung = "Paul Young";
+
+        names.add(paulYoung);
+
+        if(names.contains(paulYoung)) {
+            System.out.println("I found that paul guy!");
+        }
+
+        if(names.containsAll(nameCopy)) {
+            System.out.println("These lists are the same!");
+        } else {
+            System.out.println("They are not the same.");
+        }
+
+        // nameCopy.clear();
+        if(nameCopy.isEmpty()) {
+            System.out.println("This list is gone.");
+        }
+        System.out.print(nameCopy);
+        Object[] moreNames = new Object[4];
+
+        moreNames = nameCopy.toArray();
+
+        System.out.print(Arrays.toString(moreNames));
+
+        ArrayList stuff = new ArrayList();
+
+        stuff.add(5);
+        stuff.add(true);
+        stuff.add('*');
+        stuff.add("Pierogi");
+        System.out.println();
+        System.out.println(stuff);
+
+        Object[] newStuff = new Object[2];
+
+        newStuff = stuff.toArray();
+
+        System.out.println();
+        System.out.println(Arrays.toString(newStuff) + "this is the array");
+        System.out.println();
+        System.out.println(newStuff.getClass().getSimpleName() + "this is the name.");
+
     }
 }
